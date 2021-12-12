351ba0773111396463d33b2e04f2a5b3c6e8e6e6
JangHyeonJun2 <epfvkdlxj@naver.com>
2021-12-12 21:10:32 +0900

    [MVC 프레임워크 만들기] - View 분리 - V2 → MemberSaveControllerV1 코드변경

--- src/main/java/hello/servlet/web/frontcontroller/v1/controller/MeberSaveControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MeberSaveControllerV1.java
@@ -4,6 +4,7 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import hello.servlet.web.frontcontroller.v1.ControllerV1;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,20 +22,28 @@ public class MeberSaveControllerV1 implements ControllerV1 {
         Member member = new Member(username, age);
         memberRepository.save(member);
 
-        response.setContentType("text/html");
-        response.setCharacterEncoding("utf-8");
+        //Modelㅇㅔ 데이터를 보관한다.
+        request.setAttribute("member", member);
 
-        PrintWriter w = response.getWriter();
-        w.write("<html>\n" +
-                "<head>\n" +
-                " <meta charset=\"UTF-8\">\n" + "</head>\n" +
-                "<body>\n" +
-                "성공\n" +
-                "<ul>\n" +
-                "    <li>id="+member.getId()+"</li>\n" +
-                "    <li>username="+member.getUsername()+"</li>\n" +
-                " <li>age="+member.getAge()+"</li>\n" + "</ul>\n" +
-                "<a href=\"/index.html\">메인</a>\n" + "</body>\n" +
-                "</html>");
+        String viewPath = "WEB-INF/views/save-result.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+        response.setContentType("text/html");
+
+//        response.setContentType("text/html");
+//        response.setCharacterEncoding("utf-8");
+//
+//        PrintWriter w = response.getWriter();
+//        w.write("<html>\n" +
+//                "<head>\n" +
+//                " <meta charset=\"UTF-8\">\n" + "</head>\n" +
+//                "<body>\n" +
+//                "성공\n" +
+//                "<ul>\n" +
+//                "    <li>id="+member.getId()+"</li>\n" +
+//                "    <li>username="+member.getUsername()+"</li>\n" +
+//                " <li>age="+member.getAge()+"</li>\n" + "</ul>\n" +
+//                "<a href=\"/index.html\">메인</a>\n" + "</body>\n" +
+//                "</html>");
     }
 }
