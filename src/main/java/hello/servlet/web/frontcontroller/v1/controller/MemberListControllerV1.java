6da8dde83502b5b3a4c9985cd78590dfe83895ab
JangHyeonJun2 <epfvkdlxj@naver.com>
2022-01-01 23:58:26 +0900

    [MVC 프레임워크 만들기] - View 분리 - V2  코드변경

--- src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -4,6 +4,7 @@ import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 import hello.servlet.web.frontcontroller.v1.ControllerV1;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,35 +19,40 @@ public class MemberListControllerV1 implements ControllerV1 {
     public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         List<Member> members = memberRepository.findAll();
 
-        response.setContentType("text/html");
-        response.setCharacterEncoding("utf-8");
+        request.setAttribute("members", members);
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
 
-        PrintWriter w = response.getWriter();
-        w.write("<html>");
-        w.write("<head>");
-        w.write("    <meta charset=\"UTF-8\">");
-        w.write("    <title>Title</title>");
-        w.write("</head>");
-        w.write("<body>");
-        w.write("<a href=\"/index.html\">메인</a>");
-        w.write("<table>");
-        w.write("    <thead>");
-        w.write("    <th>id</th>");
-        w.write("    <th>username</th>");
-        w.write("    <th>age</th>");
-        w.write("    </thead>");
-        w.write("    <tbody>");
-        for (Member member : members) {
-            w.write("<tr>");
-            w.write("<td>" + member.getId() + "</td>");
-            w.write("<td>" + member.getUsername() + "</td>");
-            w.write("<td>" + member.getAge() + "</td>");
-            w.write("</tr>");
-        }
-
-        w.write("</tbody>");
-        w.write("</table>");
-        w.write("</body>");
-        w.write("</html>");
+//        response.setContentType("text/html");
+//        response.setCharacterEncoding("utf-8");
+//
+//        PrintWriter w = response.getWriter();
+//        w.write("<html>");
+//        w.write("<head>");
+//        w.write("    <meta charset=\"UTF-8\">");
+//        w.write("    <title>Title</title>");
+//        w.write("</head>");
+//        w.write("<body>");
+//        w.write("<a href=\"/index.html\">메인</a>");
+//        w.write("<table>");
+//        w.write("    <thead>");
+//        w.write("    <th>id</th>");
+//        w.write("    <th>username</th>");
+//        w.write("    <th>age</th>");
+//        w.write("    </thead>");
+//        w.write("    <tbody>");
+//        for (Member member : members) {
+//            w.write("<tr>");
+//            w.write("<td>" + member.getId() + "</td>");
+//            w.write("<td>" + member.getUsername() + "</td>");
+//            w.write("<td>" + member.getAge() + "</td>");
+//            w.write("</tr>");
+//        }
+//
+//        w.write("</tbody>");
+//        w.write("</table>");
+//        w.write("</body>");
+//        w.write("</html>");
     }
 }
